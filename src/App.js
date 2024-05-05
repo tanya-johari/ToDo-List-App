71e8cf958619f56e3ae1c4b5a214a988d24f40e5
tanyajohari <johtanya11@gmail.com>
2024-05-05 15:41:31 +0530

    [Tanya] Completed Game Checkpoint: 4 (Added empty button)

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
+import { useState } from "react";
 import Header from "./Header/Header";
 import "./styles.css";
 import ToDoList from "./ToDoList/ToDoList";
 
-const todoitmes = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", 
+const todoitems = ["Read SpringBoot", "Complete assignments", "Prepare breakfast", 
 "Sleep for 2 hours", "Take a shower", "Buy groceries"]; // Your ToDoList items
 
 export default function App() {
+
+  const [todos, setTodos] = useState(todoitems);
+
   return (
     <div className="Application">
       <Header />
-      <ToDoList todos={todoitmes} />
+      <ToDoList todos={todos} setTodos={setTodos}/>
     </div>
   );
 }
--- src/ToDoList/ToDoList.js
+++ src/ToDoList/ToDoList.js
@@ -1,13 +1,26 @@
 import React from 'react';
 
-const ToDoList = ({ todos }) => {
+const ToDoList = ({ todos, setTodos }) => {
+
+  const isEmpty = todos.length === 0; // Check if the list is empty
+
+  const handleEmptyAll = () => {
+    setTodos([]); // Set todos to an empty array to clear the list
+  };
+
   return (
-    <ul className="todo-list">
-      {/* Display only the first 5 ToDoItems */}
-      {todos.slice(0, 5).map((todo, index) => (
-        <li key={index} className="todo-item">{todo}</li>
-      ))}
-    </ul>
+    <>
+      <ul className="todo-list">
+        {isEmpty ? (
+          <li><i>Nothing to do buddy. Sleep!</i></li>
+        ) : (todos.map((todo, index) => (
+            <li key={index} className="todo-item">{todo}</li>
+          )))}
+      </ul>
+      <button className="btn-empty" onClick={handleEmptyAll}>
+        Empty
+      </button>
+    </>
   );
 };
 
